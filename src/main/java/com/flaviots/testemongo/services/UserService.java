3189315335c9285612ea6f2fcabac90432bbcad8
Flavio Tavares Silva <flavio.silva@rodassist.com.br>
2018-11-22 20:09:12 -0200

    Insercão de usuário com POST

--- src/main/java/com/flaviots/testemongo/services/UserService.java
+++ src/main/java/com/flaviots/testemongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.flaviots.testemongo.domain.User;
+import com.flaviots.testemongo.dto.UserDTO;
 import com.flaviots.testemongo.repository.UserRepository;
 import com.flaviots.testemongo.services.exception.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);		
 		return obj.orElseThrow(()-> new ObjectNotFoundException("Obejeto Não Encontrado!"));
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public 	User fromDTO(UserDTO objDto) {		
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
