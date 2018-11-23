8e685cfe48ddcbac122141d56321563494ef056e
Flavio Tavares Silva <flavio.silva@rodassist.com.br>
2018-11-23 16:25:06 -0200

    Acrescentando comentários ao post

--- src/main/java/com/flaviots/testemongo/config/Instantiation.java
+++ src/main/java/com/flaviots/testemongo/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.flaviots.testemongo.domain.Post;
 import com.flaviots.testemongo.domain.User;
 import com.flaviots.testemongo.dto.AuthorDTO;
+import com.flaviots.testemongo.dto.CommentDTO;
 import com.flaviots.testemongo.repository.PostRepository;
 import com.flaviots.testemongo.repository.UserRepository;
 
@@ -40,6 +41,13 @@ public class Instantiation implements CommandLineRunner{
 		
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou Viajar para São Paulo. Abraços!", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+		
+		CommentDTO c1 = new CommentDTO("Boa Viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha uma ótima viagem!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+		
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post2.getComments().addAll(Arrays.asList(c3));
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
