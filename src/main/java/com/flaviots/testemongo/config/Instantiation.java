34c51723bafa1ada3832ef081f1723b933acc803
Flavio Tavares Silva <flavio.silva@rodassist.com.br>
2018-11-23 13:13:04 -0200

    Projeção dos dados do Author com DTO

--- src/main/java/com/flaviots/testemongo/config/Instantiation.java
+++ src/main/java/com/flaviots/testemongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.flaviots.testemongo.domain.Post;
 import com.flaviots.testemongo.domain.User;
+import com.flaviots.testemongo.dto.AuthorDTO;
 import com.flaviots.testemongo.repository.PostRepository;
 import com.flaviots.testemongo.repository.UserRepository;
 
@@ -35,10 +36,11 @@ public class Instantiation implements CommandLineRunner{
 		User alex = new User(null, "Alex Greenn", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou Viajar para São Paulo. Abraços!", maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
-		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou Viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
 	
